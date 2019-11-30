c1cb39f7d95914827b9aa189bcafa831aff9fd12
stevevillardi <stevevillardi@gmail.com>
2019-11-29 23:05:25 -0500

    challenge 1 completed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,68 +26,41 @@ connection.connect(function(err) {
 	if (err) throw err;
 });
 
-function createProduct() {
-	console.log("Inserting a new product...\n");
-	var query = connection.query(
-		"INSERT INTO products SET ?",
-		{
-			flavor: "Rocky Road",
-			price: 3.0,
-			quantity: 50
-		},
-		function(err, res) {
-			if (err) throw err;
-			console.log(res.affectedRows + " product inserted!\n");
-			// Call updateProduct AFTER the INSERT completes
-			updateProduct();
-		}
-	);
-
-	// logs the actual query being run
-	console.log(query.sql);
-}
-
 function updateProduct(item, amount) {
-	connection.query(`SELECT * FROM products WHERE item_id = ${item}`, function(
-		err,
-		res
-	) {
-		if (err) throw err;
-		if (amount > res[0].stock_quantity) {
-			console.log(
-				`Insufficient quantity, only ${res[0].stock_quantity} in stcok!`
-			);
-			connection.end();
-		} else {
-			let updatedStock = res[0].stock_quantity - amount;
-			const query = connection.query(
-				`UPDATE products SET stock_quantity = ${updatedStock} WHERE item_id = ${item}`,
-				function(err, res) {
-					if (err) throw err;
-					console.log(res.affectedRows + " products updated!\n");
-					connection.end();
-				}
-			);
-
-			//console.log(query.sql);
-		}
-	});
-}
-
-function deleteProduct() {
-	console.log("Deleting all strawberry icecream...\n");
-	connection.query(
-		"DELETE FROM products WHERE ?",
-		{
-			flavor: "strawberry"
-		},
-		function(err, res) {
+	if (isNaN(item) || isNaN(amount)) {
+		console.log(
+			`Invalid item_id or amount type, please choose a valid product...`
+		);
+		promptUser();
+	} else {
+		connection.query(`SELECT * FROM products WHERE item_id = ${item}`, function(
+			err,
+			res
+		) {
 			if (err) throw err;
-			console.log(res.affectedRows + " products deleted!\n");
-			// Call readProducts AFTER the DELETE completes
-			readProducts();
-		}
-	);
+			if (res.length === 0) {
+				console.log(`Invalid item_id, please choose a valid product...`);
+				promptUser();
+			} else if (amount > res[0].stock_quantity) {
+				console.log(
+					`Insufficient quantity, only ${res[0].stock_quantity} in stock!`
+				);
+				promptUser();
+			} else {
+				let updatedStock = res[0].stock_quantity - amount;
+				const query = connection.query(
+					`UPDATE products SET stock_quantity = ${updatedStock} WHERE item_id = ${item}`,
+					function(err, res) {
+						if (err) throw err;
+						console.log(res.affectedRows + " products updated!\n");
+						readProducts();
+					}
+				);
+
+				//console.log(query.sql);
+			}
+		});
+	}
 }
 
 function readProducts() {
@@ -98,12 +71,15 @@ function readProducts() {
 				`ID:${obj.item_id}) ${obj.product_name}: $${obj.price} - ${obj.stock_quantity} in stock`
 			);
 		});
-		inquirer.prompt(questions).then(answers => {
-			const productID = answers.product_id;
-			const quantity = answers.product_quantity;
-			updateProduct(productID, quantity);
-		});
+		promptUser();
 	});
 }
 
+function promptUser() {
+	inquirer.prompt(questions).then(answers => {
+		const productID = answers.product_id;
+		const quantity = answers.product_quantity;
+		updateProduct(productID, quantity);
+	});
+}
 readProducts();
